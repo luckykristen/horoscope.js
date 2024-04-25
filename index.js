67d261a70e43649514849255ffb28c3be9591a8d
Kristina Kalíková <129938050+luckykristen@users.noreply.github.com>
2024-04-25 19:35:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,83 @@
+let sign = prompt('What is your sign?').toLowerCase();
+
+if (sign === 'aries') {
+    alert(`♈ No doubt you're feeling attractive and passionate today, Aries. 
+    Don't be surprised if a former lover gets in touch with you to try and rekindle the old flame. 
+    The attention is flattering, to be sure, but you're unlikely to have any desire to pursue the relationship. 
+    You're happy with what you have right now. And as they say, "If it ain't broke, don't fix it."`);
+}
+
+else if (sign === 'taurus') {
+    alert(`♉ As you get older, you're drawn more and more to spiritual matters, Taurus. 
+    It isn't so much that you're embracing any particular religion, more that you're quite curious about the supernatural and some of the ancient arts. 
+    Take some time today to visit the library or bookstore and do some reading on the subject. 
+    You might want to form a study group with other like-minded individuals.`);
+}
+
+else if (sign === 'gemini') {
+    alert(`♊ Don't hesitate to try something new today, Gemini. 
+    If you've thought about joining a book club or taking a class, do it today! 
+    You're likely to meet some interesting people and enter a new social world. 
+    You might be tentative at first, but you'll find the group warm, friendly, and eager for your input. 
+    You'll talk like old friends by the end of the second meeting. This is just the infusion of fun you need!`);
+}
+
+else if (sign === 'cancer') {
+    alert(`♋ You're likely to feel optimistic and enthusiastic about life today, Cancer. 
+    You might also feel especially sexy, a feeling you should definitely take advantage of! Why not plan a romantic evening with your loved one? 
+    Don't hesitate to talk about your travel dreams over dinner. You never know, he or she just might share your dream. 
+    Before you know it, the two of you could be winging your way to exotic lands.`);
+}
+
+else if (sign === 'leo') {
+    alert(`♌ Today is full of possibilities, Leo. You could fall in love at first sight, or at least meet someone who captures your interest! 
+    You should be feeling especially loving, passionate, and eager to devote yourself to a worthy cause or creative activity. 
+    Don't expect everything to fall into place right away. You could hit a few obstacles, but the ultimate result will be worth the bumps along the way.`);
+}
+
+else if (sign === 'virgo') {
+    alert(`♍ You're looking beautiful and feeling passionate, Virgo. If only your passions could be reciprocated! 
+    Even though there may not be anyone special in your life right now, that's no reason to not treat yourself well. 
+    Go out for a nice meal or, better yet, order take-out and dine at home, complete with music, candles, and your finest china. 
+    You should appreciate yourself even if no one else does at the moment.`);
+}
+
+else if (sign === 'libra') {
+    alert(`♎ This could be a frustrating day in the romance department, Libra. Communication is blocked at all levels. 
+    You're anxious to speak with your loved one, but simply can't reach him or her. Perhaps the phone system is malfunctioning or email is acting up. 
+    You feel as though the Universe is conspiring against you. Take heart. Your partner will contact you soon enough, and your reunion will be electric!`);
+}
+
+else if (sign === 'scorpio') {
+    alert(`♏ You have certainly felt better than you do today, Scorpio. We'll pause here while you go and fetch the aspirin. 
+    The stomachache and headache are simply the result of recent overindulgence. Don't worry - you'll be fit as a fiddle by tomorrow morning. 
+    Next time someone offers you multiple helpings of food or drink, however, you might want to consider politely declining!`);
+}
+
+else if (sign === 'sagittarius') {
+    alert(`♐ You could be feeling playful today, Sagittarius. Your romantic partner will certainly appreciate your fun-loving mood. 
+    Why not plan a fun day together doing something other than your usual routine? Rather than dinner and a movie, how about lunch and a visit to a museum or art gallery? 
+    You have both been working very hard lately. Your relationship will benefit from an injection of spontaneity.`);
+}
+
+else if (sign === 'capricorn') {
+    alert(`♑ Don't be surprised if you have a desire to redecorate. The celestial energies have put you in the mood to rearrange your house a bit. 
+    Take care to not go overboard, Capricorn. You have a tendency to turn little projects into big ones. 
+    For now, content yourself with buying some flowering plants and perhaps new area rugs and throw pillows. Leave the kitchen and bath renovations for later.`);
+}
+
+else if (sign === 'aquarius') {
+    alert(`♒ Be sure to answer all phone calls and open all letters and emails today, Aquarius. You're likely to receive some interesting news. 
+    You might get word from a former boss that you'd be perfect for a new position opening up in his or her office. 
+    Or perhaps an old lover makes tentative inquiries about renewing old bonds. Your eyebrows will rise in surprise at least once during this day. Expect the unexpected!`);
+}
+
+else if (sign === 'pisces') {
+    alert(`♓ Just as a watched pot never boils, so, too, do anxiously awaited checks or letters refuse to arrive in the mail, Pisces. 
+    Beyond double-checking that the person or company has your correct address, there isn't much you can do but continue to wait. 
+    Trust that it will arrive shortly, and then your financial difficulties will be behind you.`);
+}
+
+else {
+    alert(`❌ You made a mistake somewhere. Try again, please.`);
+}
